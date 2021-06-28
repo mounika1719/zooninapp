b23eb05057c34b96fa0187cf67591b1b9a0b2bcc
mounika1719 <71956696+mounika1719@users.noreply.github.com>
2021-06-28 16:36:45 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Typography, AppBar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
 import VideoPlayer from './components/VideoPlayer';
 import Options from './components/Options';
 import Notifications from './components/Notifications';
-
 const useStyles = makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
@@ -16,7 +14,6 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     width: '600px',
     border: '2px solid black',
-
     [theme.breakpoints.down('xs')]: {
       width: '90%',
     },
@@ -32,7 +29,6 @@ const useStyles = makeStyles((theme) => ({
 
 const App = () => {
   const classes = useStyles();
-
   return (
     <div className={classes.wrapper}>
       <AppBar className={classes.appBar} position="static" color="inherit">
@@ -45,5 +41,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
